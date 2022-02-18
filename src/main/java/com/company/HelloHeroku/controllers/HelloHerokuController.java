c3bc012dce9ea79ff70bbdedc07613f79ab9a0ae
katredford <katharine.redford@gmail.com>
2022-02-18 11:29:25 -0600

    first commit

---
+++ src/main/java/com/company/HelloHeroku/controllers/HelloHerokuController.java
@@ -0,0 +1,14 @@
+package com.company.HelloHeroku.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloHerokuController {
+
+    @GetMapping("/hello")
+
+    public String hello() {
+        return "Hello heroku";
+    }
+}
